78d9f0b8aec58d2f131f689322890c2db79284ef
Pierre Schmitz <pierre@archlinux.de>
2022-02-12 15:26:52 +0100

    Initial import of implementation

--- extend.php
+++ extend.php
@@ -12,11 +12,26 @@
 namespace ArchLinux\ClickImage;
 
 use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+use s9e\TextFormatter\Configurator\Items\Tag;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
-    
-    new Extend\Locales(__DIR__.'/locale'),
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/less/forum.less'),
+
+    new Extend\Locales(__DIR__ . '/locale'),
+
+    (new Extend\Formatter())
+        ->configure(function (Configurator $config) {
+            // Override img tag
+            // https://github.com/s9e/TextFormatter/blob/master/src/Plugins/BBCodes/Configurator/repository.xml#L216-L221
+            /** @var Tag $imgTag */
+            $imgTag = $config->tags['IMG'];
+            $imgTag->setTemplate(
+                '<click-img src="{@src}" title="{@title}" alt="{@alt}">'
+                . '<xsl:copy-of select="@height"/><xsl:copy-of select="@width"/>'
+                . '</click-img>'
+            );
+        }),
 ];
